26f48f7c9e3c1e96e57ce1807f0da2cd235925ca
Ethan Rozee <et218892@dal.ca>
2024-02-03 12:23:05 -0400

    Setup a database and its connectivity with the mobile application

---
+++ app/src/main/java/com/example/csci3130_group_3/DatabaseExampleActivity.java
@@ -0,0 +1,40 @@
+package com.example.csci3130_group_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+public class DatabaseExampleActivity extends AppCompatActivity {
+
+    Database db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_example);
+
+        // The easiest way to run this example is to change the launch activity in the android manifest file.
+        // Refer to this source for help: https://stackoverflow.com/questions/3631982/change-applications-starting-activity
+
+        db = new MyFirebaseDatabase(this);
+        Employer emp = new Employer("Parth", "alkjo139ksdf09812n");
+
+        Button sendBtn = findViewById(R.id.sendButton);
+        sendBtn.setOnClickListener(v -> {
+            db.write("test", emp);
+            Log.d("send", "sent");
+        });
+
+        Button recvBtn = findViewById(R.id.recvButton);
+        recvBtn.setOnClickListener(v -> {
+            db.read(
+                "test",
+                User.class,
+                user -> Log.d("recv", user.getName()),
+                error -> Log.d("recv", error)
+            );
+        });
+    }
+}
