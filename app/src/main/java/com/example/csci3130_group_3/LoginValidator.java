96766a783a9586038d24e0130a0e269347712290
pdoshi <pr882442@dal.ca>
2024-02-05 19:41:42 -0400

    Revert "TEST 1 : created first test for null email ID"
    This reverts commit a2ba562e3cf1ee4c284e8376b790d618cfcc6d52.

--- app/src/main/java/com/example/csci3130_group_3/LoginValidator.java
+++
@@ -1,8 +0,0 @@
-package com.example.csci3130_group_3;
-
-public class LoginValidator {
-
-    public static boolean isValidEmail(String email){
-        return true;
-    }
-}
--- app/src/test/java/com/example/csci3130_group_3/ExampleUnitTest.java
+++ app/src/test/java/com/example/csci3130_group_3/ExampleUnitTest.java
@@ -1,18 +1,17 @@
 package com.example.csci3130_group_3;
 
 import org.junit.Test;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class LoginValidatorUnitTest {
+public class ExampleUnitTest {
     @Test
-    public void checkEmailNull() {
-        assertFalse(LoginValidator.isValidEmail(null));
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
     }
-
 }
