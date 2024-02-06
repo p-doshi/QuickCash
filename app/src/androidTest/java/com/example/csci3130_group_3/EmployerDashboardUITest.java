c219cf054ecced7181f6bde078ef2b7ea050d03e
Yuki Law <Yuki.Law@dal.ca>
2024-02-06 15:18:37 -0400

    Set up and added UI test for employer dashboard

--- app/src/androidTest/java/com/example/csci3130_group_3/EmployerDashboardUITest.java
+++ app/src/androidTest/java/com/example/csci3130_group_3/EmployerDashboardUITest.java
@@ -1,4 +1,51 @@
 package com.example.csci3130_group_3;
 
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+//need to change the name according to your file
+//import com.example.csci3130_group_3.[employerDashboard];
+
 public class EmployerDashboardUITest {
+    private static final int LAUNCH_TIMEOUT = 5000;
+    final String launcherPackage = "com.example.csci3130_group_3";
+    private UiDevice device;
+
+    @Before
+    public void setup() {
+        device = UiDevice.getInstance(getInstrumentation());
+        Context context = ApplicationProvider.getApplicationContext();
+        final Intent appIntent = context.getPackageManager().getLaunchIntentForPackage(launcherPackage);
+        appIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(appIntent);
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void checkIfEmployerDashboardPageIsVisible() {
+        UiObject employerMapIcon = device.findObject(new UiSelector().resourceId("employerMapPage"));
+        assertTrue(employerMapIcon.exists());
+        UiObject employerSearchIcon = device.findObject(new UiSelector().resourceId("employerSearchPage"));
+        assertTrue(employerSearchIcon.exists());
+        UiObject employerReceiptIcon = device.findObject(new UiSelector().resourceId("employerReceiptPage"));
+        assertTrue(employerReceiptIcon.exists());
+        UiObject employerProfileIcon = device.findObject(new UiSelector().resourceId("employerProfilePage"));
+        assertTrue(employerProfileIcon.exists());
+    }
 }
