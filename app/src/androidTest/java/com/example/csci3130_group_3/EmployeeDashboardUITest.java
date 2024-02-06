cab103b6f721eff40e46d3be9804206a3ef86901
Yuki Law <Yuki.Law@dal.ca>
2024-02-06 14:48:21 -0400

    Added UI test for employee dashboard page

---
+++ app/src/androidTest/java/com/example/csci3130_group_3/EmployeeDashboardUITest.java
@@ -0,0 +1,49 @@
+package com.example.csci3130_group_3;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+public class EmployeeDashboardUITest {
+    private static final int LAUNCH_TIMEOUT = 5000;
+    final String launcherPackage = "com.example.csci3130_group_3";
+    private UiDevice device;
+
+    @Before
+    public void setup() {
+        device = UiDevice.getInstance(getInstrumentation());
+        Context context = ApplicationProvider.getApplicationContext();
+        final Intent appIntent = context.getPackageManager().getLaunchIntentForPackage(launcherPackage);
+        appIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(appIntent);
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void checkIfEmployeeDashboardPageIsVisible() {
+        UiObject mapIcon = device.findObject(new UiSelector().resourceId("mapPage"));
+        assertTrue(mapIcon.exists());
+        UiObject searchIcon = device.findObject(new UiSelector().resourceId("searchPage"));
+        assertTrue(searchIcon.exists());
+        UiObject receiptIcon = device.findObject(new UiSelector().textContains("receiptPage"));
+        assertTrue(receiptIcon.exists());
+        UiObject profileIcon = device.findObject(new UiSelector().text("profilePage"));
+        assertTrue(profileIcon.exists());
+    }
+}
---
+++ app/src/androidTest/java/com/example/csci3130_group_3/EmployerDashboardUITest.java
@@ -0,0 +1,4 @@
+package com.example.csci3130_group_3;
+
+public class EmployerDashboardUITest {
+}
