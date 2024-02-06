91638f35b4f268268e50c4a267c38459091a6980
Hayley Vezeau <hy866360@dal.ca>
2024-02-06 15:02:02 -0400

    Created UI test cases for user story 3 - select role

---
+++ app/src/androidTest/java/com/example/csci3130_group_3/ChooseRoleUITests.java
@@ -0,0 +1,68 @@
+package com.example.csci3130_group_3;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.content.Intent;
+
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+
+import org.junit.Before;
+
+/**
+ * Tests for user story 3 - Choose Roles
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class ChooseRoleUITests {
+
+    private static final int LAUNCH_TIMEOUT = 5000;
+    final String launcherPackage = "com.example.csci3130_group_3";
+    private UiDevice device;
+
+    @Before
+    public void setup() {
+        device = UiDevice.getInstance(getInstrumentation());
+        Context context = ApplicationProvider.getApplicationContext();
+        final Intent appIntent = context.getPackageManager().getLaunchIntentForPackage(launcherPackage);
+        appIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(appIntent);
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void checkIfEmployeeBoxVisible(){
+
+
+        UiObject employeeBox = device.findObject(new UiSelector().text("Employee"));
+        assertTrue(employeeBox.exists());
+    }
+
+    @Test
+    public void checkIfEmployerBoxVisable(){
+
+        UiObject employerBox = device.findObject(new UiSelector().textContains("Employer"));
+        assertTrue(employerBox.exists());
+    }
+
+    @Test
+    public void checkIfConfirmBoxVisable(){
+
+        UiObject employerBox = device.findObject(new UiSelector().textContains("Confirm"));
+        assertTrue(employerBox.exists());
+    }
+}
