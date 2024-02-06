2fd2f9d669f01d6993caa5f2870db6e4c0991f35
rozee <et218892@dal.ca>
2024-02-05 20:23:23 -0400

    Fixed TEST 1

--- app/src/androidTest/java/com/example/csci3130_group_3/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/csci3130_group_3/ExampleInstrumentedTest.java
@@ -1,16 +1,24 @@
 package com.example.csci3130_group_3;
 
 
+import static android.app.PendingIntent.getActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.action.ViewActions.typeTextIntoFocusedView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+import android.view.View;
 
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.IdlingResource;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.After;
@@ -20,25 +28,24 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-
-    private IdlingResource idlingResource;
+    public ActivityScenario<MainActivity> scenario;
+    public View decorView;
 
     @Before
-
+    public void setup() {
+        scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.onActivity((ActivityScenario.ActivityAction<MainActivity>) activity -> {
+            decorView = activity.getWindow().getDecorView();
+        });
+    }
 
     @Test
     public void testValidCredentials() {
-        onView(withId(R.id.editTextText)).perform(typeText("5"));
-        onView(withId(R.id.etPassword)).perform(typeText("hi"));
-        onView(withId(R.id.button2)).perform(click());
-        onView(withText(R.string.TOAST_STRING)).inRoot(withDecorView(not(is(getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+        onView(withId(R.id.emailaddress)).perform(typeText("5")).perform(closeSoftKeyboard());
+        onView(withId(R.id.etPassword)).perform(typeText("hi")).perform(closeSoftKeyboard());
+        onView(withId(R.id.Continue)).perform(click());
+        onView(withText(R.string.TOAST_STRING))
+            .inRoot(withDecorView(not(is(decorView))))
+            .check(matches(isDisplayed()));
     }
-
-    @After
-    public void tearDown() {
-        if (idlingResource != null) {
-            IdlingRegistry.getInstance().unregister(idlingResource);
-        }
-    }
-
 }
--- app/src/main/java/com/example/csci3130_group_3/MainActivity.java
+++ app/src/main/java/com/example/csci3130_group_3/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.csci3130_group_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailInput;
+    TextInputEditText passwordInput;
+    Toast toastMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the inputs.
+        emailInput = findViewById(R.id.emailaddress);
+        passwordInput = findViewById(R.id.etPassword);
+
+        // Create a toast message.
+        toastMsg = Toast.makeText(this, getResources().getString(R.string.TOAST_STRING), Toast.LENGTH_SHORT);
+
+        // Set the button functionality.
+        Button continueButton = findViewById(R.id.Continue);
+        continueButton.setOnClickListener(v -> {
+            validateAndLogin();
+        });
+    }
+
+    void validateAndLogin() {
+        // Get the text inputs here.
+
+        // Email and password validation here.
+
+        // If we haven't returned yet, then valid credentials.
+        toastMsg.show();
     }
 }
