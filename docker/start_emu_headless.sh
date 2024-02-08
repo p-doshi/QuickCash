98d0377044b29916f6990cc93a4dff6ff3310389
rozee <et218892@dal.ca>
2024-02-07 21:28:53 -0400

    Trying a new approach

--- docker/start_emu_headless.sh
+++ docker/start_emu_headless.sh
@@ -12,13 +12,8 @@ function check_hardware_acceleration() {
     if [[ "$HW_ACCEL_OVERRIDE" != "" ]]; then
         hw_accel_flag="$HW_ACCEL_OVERRIDE"
     else
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-            # macOS-specific hardware acceleration check
-            HW_ACCEL_SUPPORT=$(sysctl -a | grep -E -c '(vmx|svm)')
-        else
-            # generic Linux hardware acceleration check
-            HW_ACCEL_SUPPORT=$(grep -E -c '(vmx|svm)' /proc/cpuinfo)
-        fi
+        # generic Linux hardware acceleration check
+        HW_ACCEL_SUPPORT=$(grep -E -c '(vmx|svm)' /proc/cpuinfo)
 
         if [[ $HW_ACCEL_SUPPORT == 0 ]]; then
             hw_accel_flag="-accel off"
@@ -35,20 +30,9 @@ hw_accel_flag=$(check_hardware_acceleration)
 
 function launch_emulator () {
   adb devices | grep emulator | cut -f1 | xargs -I {} adb -s "{}" emu kill
-  options="@${emulator_name} -no-window -no-snapshot -noaudio -no-boot-anim -memory 2048 ${hw_accel_flag} -camera-back none"
-  if [[ "$OSTYPE" == *linux* ]]; then
-    echo "${OSTYPE}: emulator ${options} -gpu off"
-    nohup emulator $options -gpu off &
-  fi
-  if [[ "$OSTYPE" == *darwin* ]] || [[ "$OSTYPE" == *macos* ]]; then
-    echo "${OSTYPE}: emulator ${options} -gpu swiftshader_indirect"
-    nohup emulator $options -gpu swiftshader_indirect &
-  fi
-
-  if [ $? -ne 0 ]; then
-    echo "Error launching emulator"
-    return 1
-  fi
+  options="@${emulator_name} -no-window -no-snapshot -noaudio -no-boot-anim -memory 2048 ${hw_accel_flag} -camera-back none -gpu off"
+  echo "${OSTYPE}: emulator ${options}"
+  nohup emulator $options &
 }
 
 
