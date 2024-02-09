b99fe511d36f4b0a99cc0b964e4ebed5b1e3f2d9
rozee <et218892@dal.ca>
2024-02-09 14:55:54 -0400

    Added release and debug app checking sub classes for Firebase database

---
+++ app/src/main/java/com/example/csci3130_group_3/MyDebugFirebaseDatabase.java
@@ -0,0 +1,23 @@
+package com.example.csci3130_group_3;
+
+import android.content.Context;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
+
+public class MyDebugFirebaseDatabase extends MyFirebaseDatabase {
+    MyDebugFirebaseDatabase(Context context) {
+        super(initApp(context));
+    }
+
+    private static Context initApp(Context context) {
+        // Verify our app to use the database.
+        FirebaseApp.initializeApp(context);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+            DebugAppCheckProviderFactory.getInstance());
+
+        return context;
+    }
+}
