a31a8927171a8faca159a0f73e7c5fd494b5ea28
Hayley Vezeau <hy866360@dal.ca>
2024-02-14 11:09:36 -0400

    Rework the tests to be espresso tests, I don't think they are properly written yet

---
+++ app/src/androidTest/java/com/example/csci3130_group_3/ChooseRoleEspressoTest.java
@@ -0,0 +1,52 @@
+package com.example.csci3130_group_3;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+import static java.util.regex.Pattern.matches;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.ViewAssertion;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+public class ChooseRoleEspressoTest {
+    ActivityScenario<ChooseRoleActivity> activityScenario;
+    @Before
+    public void setup(){
+        activityScenario=ActivityScenario.launch(ChooseRoleActivity.class);
+        activityScenario.onActivity(activity -> {
+        });
+    }
+
+    @Test
+    public void employeeButtonExists() {
+        onView(withId(R.id.employeeButton)).perform(click());
+        onView(withId(R.id.employeeButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void employerButtonExists() {
+        onView(withId(R.id.employerButton)).perform(click());
+        onView(withId(R.id.employerButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void roleConfirmationButtonExists() {
+        onView(withId(R.id.chooseRoleConfirm)).perform(click());
+        onView(withId(R.id.chooseRoleConfirm)).check(matches(isDisplayed()));
+    }
+
+
+}
