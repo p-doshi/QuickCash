7071dcbdaeaf0d1a75ecc903ba3437e73ed1caba
Hayley Vezeau <hy866360@dal.ca>
2024-02-15 17:01:15 -0400

    minor change

--- app/src/main/java/com/example/csci3130_group_3/EmployerDashboard.java
+++ app/src/main/java/com/example/csci3130_group_3/EmployerDashboard.java
@@ -3,11 +3,15 @@ package com.example.csci3130_group_3;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class EmployerDashboard extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.dashboard_employer);
+
+        BottomNavigationView employerNavView = findViewById(R.id.employeeBottomNavView);
 
     }
 
