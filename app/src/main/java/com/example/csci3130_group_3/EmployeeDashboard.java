29f94bc9532b0d09d5a7939c0baf93f78b52b188
Hayley Vezeau <hy866360@dal.ca>
2024-02-15 16:33:33 -0400

    delete incorrect test class and minor update to creation of employee dashboard

--- app/src/main/java/com/example/csci3130_group_3/EmployeeDashboard.java
+++ app/src/main/java/com/example/csci3130_group_3/EmployeeDashboard.java
@@ -2,12 +2,16 @@ package com.example.csci3130_group_3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EmployeeDashboard extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.dashboard_employee);
+
+
+        BottomNavigationView employeeNavView = findViewById(R.id.employeeBottomNavView);
 
     }
 
