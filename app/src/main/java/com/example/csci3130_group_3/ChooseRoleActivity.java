a0483092a1d3b17b09c69c78b2819af4e36065d2
Yuki Law <Yuki.Law@dal.ca>
2024-02-15 20:35:57 -0400

    Change hardcoded String to string resources and make variable name more specific

--- app/src/main/java/com/example/csci3130_group_3/ChooseRoleActivity.java
+++ app/src/main/java/com/example/csci3130_group_3/ChooseRoleActivity.java
@@ -2,14 +2,12 @@ package com.example.csci3130_group_3;
 
 //import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ChooseRoleActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ChooseRoleActivity extends AppCompatActivity {
     private String role;
 
     @Override
@@ -22,9 +20,9 @@ public class ChooseRoleActivity extends AppCompatActivity implements AdapterView
         Button chooseEmployeeButton = findViewById(R.id.employeeButton);
         Button confirmRoleButton = findViewById(R.id.chooseRoleConfirm);
 
-        chooseEmployerButton.setOnClickListener(view -> role = "Employer");
+        chooseEmployerButton.setOnClickListener(view -> role = getResources().getString(R.string.employer));
 
-        chooseEmployeeButton.setOnClickListener(view -> role = "Employee");
+        chooseEmployeeButton.setOnClickListener(view -> role = getResources().getString(R.string.employee));
 
         confirmRoleButton.setOnClickListener(view -> {
             if (role == null) {
@@ -36,28 +34,20 @@ public class ChooseRoleActivity extends AppCompatActivity implements AdapterView
 
     }
 
-    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-        // No action needed in this case
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-        // No action needed in this case
-    }
-
     protected void move2DashboardWindow(String role) {
         //commented code are for when all the activities are connected
-        //Intent dbIntent;
+        //Intent dashboardIntent;
 
         if (role.equals("Employer")) {
-            //dbIntent = new Intent(getBaseContext(), EmployerDashboard.class);
+            //dashboardIntent = new Intent(getBaseContext(), EmployerDashboard.class);
             Toast.makeText(getApplicationContext(), "Switch to Employer Dashboard", Toast.LENGTH_LONG).show();
-        } else {
-            //dbIntent = new Intent(getBaseContext(), EmployeeDashboard.class);
+        } else if (role.equals("Employee")) {
+            //dashboardIntent = new Intent(getBaseContext(), EmployeeDashboard.class);
             Toast.makeText(getApplicationContext(), "Switch to Employee Dashboard", Toast.LENGTH_LONG).show();
-
+        } else {
+            Toast.makeText(getApplicationContext(), "An error has occurred", Toast.LENGTH_LONG).show();
         }
 
-        //startActivity(dbIntent);
+        //startActivity(dashboardIntent);
     }
 }
