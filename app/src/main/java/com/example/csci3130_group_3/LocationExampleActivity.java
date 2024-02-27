e5320e6991fc7a72a3de0a9c9aba5e9f80dbb80a
rozee <et218892@dal.ca>
2024-02-27 17:42:40 -0400

    Removed another unused import

--- app/src/main/java/com/example/csci3130_group_3/LocationExampleActivity.java
+++ app/src/main/java/com/example/csci3130_group_3/LocationExampleActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Toast;
