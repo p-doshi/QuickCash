efc6449d5e3e4b0bffe4f27cf74d3723cdf3f242
ethroz <48915432+ethroz@users.noreply.github.com>
2024-03-11 16:55:50 -0300

    Created Search page UI

---
+++ app/src/main/java/dal/cs/quickCash3/search/SearchActivity.java
@@ -0,0 +1,15 @@
+package dal.cs.quickCash3.search;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dal.cs.quickCash3.R;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
