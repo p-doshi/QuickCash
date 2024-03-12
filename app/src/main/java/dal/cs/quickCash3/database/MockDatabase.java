b21de29c5aa5036f4f7a79540fed1b06a4352b15
ethroz <48915432+ethroz@users.noreply.github.com>
2024-03-12 15:27:43 -0300

    Refactored and wrote tests for the mock database

--- app/src/main/java/dal/cs/quickCash3/database/MockDatabase.java
+++ app/src/main/java/dal/cs/quickCash3/database/MockDatabase.java
@@ -1,43 +1,23 @@
 package dal.cs.quickCash3.database;
 
+import static dal.cs.quickCash3.database.DatabaseHelper.splitLocationIntoKeys;
+
 import androidx.annotation.NonNull;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.function.Consumer;
 
 public class MockDatabase implements Database {
-    public static class MapType extends TreeMap<String, Object> {}
-    private MapType data;
+    private final Map<String, Object> data = new MapType();
 
-    public static List<String> splitLocationIntoKeys(String location) {
-        ArrayList<String> strings = new ArrayList<>();
-
-        String currentLocation = location;
-        for (int slashPosition = currentLocation.indexOf('/');
-             slashPosition >= 0;
-             slashPosition = currentLocation.indexOf('/')) {
-
-            String key = currentLocation.substring(0, slashPosition);
-            currentLocation = currentLocation.substring(slashPosition + 1);
-
-            if (!key.isEmpty()) {
-                strings.add(key);
-            }
-        }
-
-        if (!currentLocation.isEmpty()) {
-            strings.add(currentLocation);
-        }
-
-        return strings;
+    // Let's create a class to avoid generic type cast warnings.
+    public static class MapType extends TreeMap<String, Object> {
+        private static final long serialVersionUID = 1L;
     }
 
-    private Object getData(String location) {
-        List<String> keys = splitLocationIntoKeys(location);
-        return recursiveGet(data, keys);
-    }
+    public MockDatabase() {}
 
     private Object recursiveGet(Object obj, @NonNull List<String> keys) {
         if (keys.isEmpty()) {
@@ -49,7 +29,7 @@ public class MockDatabase implements Database {
         if (!(obj instanceof MapType)) {
             throw new IllegalArgumentException("Key not found: " + key);
         }
-        MapType map = (MapType)obj;
+        Map<String, Object> map = (MapType)obj;
 
         if (!map.containsKey(key)) {
             throw new IllegalArgumentException("Key not found: " + key);
@@ -59,18 +39,7 @@ public class MockDatabase implements Database {
         return recursiveGet(nestedData, keys);
     }
 
-    private <T> void setData(String location, T value) {
-        List<String> keys = splitLocationIntoKeys(location);
-        if (keys.isEmpty()) {
-            throw new IllegalArgumentException("Must provide a location to write data");
-        }
-        if (data == null) {
-            data = new MapType();
-        }
-        recursiveSet(data, keys, value);
-    }
-
-    private <T> void recursiveSet(MapType map, @NonNull List<String> keys, T value) {
+    private <T> void recursiveSet(@NonNull Map<String, Object> map, @NonNull List<String> keys, T value) {
         assert !keys.isEmpty();
 
         // Get the next key we are looking for.
@@ -83,7 +52,7 @@ public class MockDatabase implements Database {
             return;
         }
 
-        MapType nestedMap;
+        Map<String, Object> nestedMap;
         Object nestedData = map.get(key);
         if (nestedData instanceof MapType) {
             nestedMap = (MapType)nestedData;
@@ -104,10 +73,16 @@ public class MockDatabase implements Database {
     @Override
     public <T> void write(String location, T value, Runnable successFunction, Consumer<String> errorFunction) {
         try {
-            setData(location, value);
+            List<String> keys = splitLocationIntoKeys(location);
+            if (keys.isEmpty()) {
+                throw new IllegalArgumentException("Must provide a location to write data");
+            }
+
+            recursiveSet(data, keys, value);
+
             successFunction.run();
         }
-        catch (Exception exception) {
+        catch (IllegalArgumentException exception) {
             errorFunction.accept(exception.getMessage());
         }
     }
@@ -115,12 +90,34 @@ public class MockDatabase implements Database {
     @Override
     public <T> void read(String location, Class<T> type, Consumer<T> readFunction, Consumer<String> errorFunction) {
         try {
-            Object obj = getData(location);
+            List<String> keys = splitLocationIntoKeys(location);
+            Object obj = recursiveGet(data, keys);
             T value = type.cast(obj);
             readFunction.accept(value);
         }
-        catch (Exception exception) {
+        catch (ClassCastException | IllegalArgumentException exception) {
             errorFunction.accept(exception.getMessage());
         }
+    }
+
+    @Override
+    public <T> int addListener(@NonNull String location, @NonNull Class<T> type, @NonNull Consumer<T> readFunction, @NonNull Consumer<String> errorFunction) {
+        // TODO
+        return -1;
+    }
+
+    @Override
+    public void removeListener(int listenerId) {
+        // TODO
+    }
+
+    @Override
+    public void delete(@NonNull String location, @NonNull Consumer<String> errorFunction) {
+        delete(location, () -> {}, errorFunction);
+    }
+
+    @Override
+    public void delete(@NonNull String location, @NonNull Runnable successFunction, @NonNull Consumer<String> errorFunction) {
+        // TODO
     }
 }
