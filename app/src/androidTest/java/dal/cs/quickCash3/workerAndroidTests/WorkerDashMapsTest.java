0f02179373c01b2d74487ef2de96c86cbcdd9933
guandong <guandong@timberlea.cs.dal.ca>
2024-03-11 22:40:02 -0300

    Start working on the code for test.

--- app/src/androidTest/java/dal/cs/quickCash3/workerAndroidTests/WorkerDashMapsTest.java
+++ app/src/androidTest/java/dal/cs/quickCash3/workerAndroidTests/WorkerDashMapsTest.java
@@ -1,4 +1,54 @@
 package dal.cs.quickCash3.workerAndroidTests;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import android.Manifest;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.GrantPermissionRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObject2;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiScrollable;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+@RunWith(AndroidJUnit4.class)
 public class WorkerDashMapsTest {
+    public String launcherPackage = "dal.cs.quickCash3.worker";
+    public int TIME_OUT = 5000;
+    UiDevice device;
+
+    @Before
+    public void setup() {
+        device = UiDevice.getInstance(getInstrumentation());
+        Context context = ApplicationProvider.getApplicationContext();
+        Intent launcherIntent = context.getPackageManager().getLaunchIntentForPackage(launcherPackage);
+        launcherIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(launcherIntent);
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), TIME_OUT);
+    }
 }
