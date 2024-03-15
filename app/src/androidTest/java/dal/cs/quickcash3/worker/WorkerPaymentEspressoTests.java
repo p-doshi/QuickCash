4c78ff9c53581e3bfc48423598f8b0b560be1109
Hayley Vezeau <hy866360@dal.ca>
2024-03-15 19:03:17 -0300

    created tests for user story 38 - As an employee, I want an online payment system so that I can get paid asap.

---
+++ app/src/androidTest/java/dal/cs/quickcash3/worker/WorkerPaymentEspressoTests.java
@@ -0,0 +1,64 @@
+package dal.cs.quickcash3.worker;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+
+import dal.cs.quickcash3.R;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WorkerPaymentEspressoTests {
+
+    public ActivityScenario<WorkerPayPalActivity> scenario;
+    private UiDevice device;
+
+
+    @Before
+    public void setup() {
+        scenario = ActivityScenario.launch(WorkerPayPalActivity.class);
+        device = UiDevice.getInstance(getInstrumentation());
+        scenario.onActivity(activity -> {
+
+        });
+    }
+
+    @Test
+    public void showPayConfirmationButton(){
+        onView(ViewMatchers.withId(R.id.seePayStatus)).perform(click());
+    }
+
+    @Test
+    public void moveToPayConfirmationPage() throws UiObjectNotFoundException {
+        onView(ViewMatchers.withId(R.id.seePayStatus)).perform(click());
+        UiObject paymentStatusButton = device.findObject(R.id.seePayStatus);
+        paymentStatusButton.clickAndWaitForNewWindow();
+    }
+
+    @Test
+    public void showPaymentStatus(){
+        onView(ViewMatchers.withId(R.id.seePayStatus)).perform(click());
+        UiObject statusMessage = device.findObject(R.id.statusMessage);
+        assertTrue(statusMessage.exists());
+        UiObject payID = device.findObject(R.id.payID);
+        assertTrue(payID.exists());
+    }
+
+}
---
+++ app/src/main/java/dal/cs/quickcash3/worker/WorkerPayPalActivity.java
@@ -0,0 +1,6 @@
+package dal.cs.quickcash3.worker;
+
+import android.app.Activity;
+
+public class WorkerPayPalActivity extends Activity {
+}
