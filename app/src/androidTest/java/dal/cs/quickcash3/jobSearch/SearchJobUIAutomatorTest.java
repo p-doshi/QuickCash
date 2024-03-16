c7594e0303181fb6fac8c16ae3f65c7913449b1c
pdoshi <pr882442@dal.ca>
2024-03-16 13:06:14 -0300

    UIAutomator tests added

--- app/src/androidTest/java/dal/cs/quickcash3/jobSearch/SearchJobUIAutomatorTest.java
+++ app/src/androidTest/java/dal/cs/quickcash3/jobSearch/SearchJobUIAutomatorTest.java
@@ -3,33 +3,34 @@ package dal.cs.quickcash3.jobSearch;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.assertTrue;
 
-import androidx.test.core.app.ActivityScenario;
+import android.Manifest;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.rule.GrantPermissionRule;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiScrollable;
 import androidx.test.uiautomator.UiSelector;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.After;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
-import dal.cs.quickcash3.R;
-import dal.cs.quickcash3.login.LoginActivity;
 import dal.cs.quickcash3.worker.WorkerDashboard;
 
 public class SearchJobUIAutomatorTest {
-    private static final String EMAIL = "Email";
-    private static final String PASSWORD = "Password";
-    private static final String CONTINUE = "Continue";
-    private static final String WELCOME = "Welcome";
-    private static final String PARTH_GMAIL = "parthdoshi135@gmail.com";
+
+    private static final String JOB = "Job Search";
+
     @Rule
     public final ActivityScenarioRule<WorkerDashboard> activityRule =
             new ActivityScenarioRule<>(WorkerDashboard.class);
+    @Rule
+    public GrantPermissionRule permissionRule =
+            GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION);
     private final UiDevice device = UiDevice.getInstance(getInstrumentation());
 
     @After
@@ -38,61 +39,45 @@ public class SearchJobUIAutomatorTest {
         FirebaseAuth.getInstance().signOut();
     }
 
-    private void relaunchApp() {
-        ActivityScenario<WorkerDashboard> scenario = activityRule.getScenario();
-        scenario.recreate();
-    }
 
     @Test
     public void checkIfLandingPageIsVisible() throws UiObjectNotFoundException {
-        UiObject registerButton = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/workerSearchPage"));
-        registerButton.clickAndWaitForNewWindow();
+        UiObject searchPage = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/workerSearchPage"));
+        searchPage.clickAndWaitForNewWindow();
         UiObject searchBox = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/searchBar"));
         assertTrue(searchBox.exists());
         UiObject filterIcon = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/filterIcon"));
         assertTrue(filterIcon.exists());
     }
 
-    //below tests wont work until pages connected :(
-    @Test
-    @Ignore("only after pages connected")
-    public void checkIfMovedToDashboard() throws UiObjectNotFoundException {
 
-        UiObject emailIDBox = device.findObject(new UiSelector().textContains(EMAIL));
-        emailIDBox.setText(PARTH_GMAIL);
-        UiObject passwordBox = device.findObject(new UiSelector().textContains(PASSWORD));
-        passwordBox.setText(PASSWORD);
-        UiObject registerButton = device.findObject(new UiSelector().text(CONTINUE));
-        registerButton.clickAndWaitForNewWindow();
-        UiObject welcomeLabel = device.findObject(new UiSelector().textContains(WELCOME));
+    @Test
+    public void checkIfMovedToSearchFilter() throws UiObjectNotFoundException {
+        UiObject searchPage = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/workerSearchPage"));
+        searchPage.clickAndWaitForNewWindow();
+        UiObject filterIcon = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/filterIcon"));
+        assertTrue(filterIcon.exists());
+        filterIcon.clickAndWaitForNewWindow();
+        UiObject welcomeLabel = device.findObject(new UiSelector().textContains(JOB));
         assertTrue(welcomeLabel.exists());
     }
 
-    @Test
-    @Ignore("only after pages connected")
-    public void checkIfRememberMeWorks() throws UiObjectNotFoundException {
-        UiObject emailIDBox = device.findObject(new UiSelector().textContains(EMAIL));
-        emailIDBox.setText(PARTH_GMAIL);
-        UiObject passwordBox = device.findObject(new UiSelector().textContains(PASSWORD));
-        passwordBox.setText(PASSWORD);
-        UiObject checkbox = device.findObject(new UiSelector().textContains("Remember"));
-        checkbox.click();
-        UiObject registerButton = device.findObject(new UiSelector().text(CONTINUE));
-        registerButton.click();
-        relaunchApp();
-    }
 
     @Test
-    @Ignore("only after pages connected")
-    public void checkIfMovedToSignUpPage() throws UiObjectNotFoundException {
-
-        UiObject emailIDBox = device.findObject(new UiSelector().textContains(EMAIL));
-        emailIDBox.setText(PARTH_GMAIL);
-        UiObject passwordBox = device.findObject(new UiSelector().textContains(PASSWORD));
-        passwordBox.setText(PASSWORD);
-        UiObject registerButton = device.findObject(new UiSelector().text(CONTINUE));
-        registerButton.clickAndWaitForNewWindow();
-        UiObject welcomeLabel = device.findObject(new UiSelector().textContains(WELCOME));
+    public void checkIfMovedToJobSearchPage() throws UiObjectNotFoundException {
+        UiObject searchPage = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/workerSearchPage"));
+        searchPage.clickAndWaitForNewWindow();
+        UiObject filterIcon = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/filterIcon"));
+        assertTrue(filterIcon.exists());
+        filterIcon.clickAndWaitForNewWindow();
+        UiObject welcomeLabel = device.findObject(new UiSelector().textContains(JOB));
         assertTrue(welcomeLabel.exists());
+        UiScrollable scrollable = new UiScrollable(new UiSelector().scrollable(true));
+        scrollable.setAsVerticalList();
+        scrollable.scrollForward();
+        UiObject searchButton = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/searchButton"));
+        searchButton.clickAndWaitForNewWindow();
+        UiObject searchBox = device.findObject(new UiSelector().resourceId("dal.cs.quickcash3:id/searchBar"));
+        assertTrue(searchBox.exists());
     }
 }
