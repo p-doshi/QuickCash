be7ce38cba59f3cd148fcfad73a014d323541902
Mathew Smith <mt965603@dal.ca>
2024-03-16 06:38:00 -0300

    Tests work on my phone however I will checkout other branch for the implemented solution to enabling location permission just for the sake of tests

--- app/src/androidTest/java/dal/cs/quickCash3/workerAndroidTests/WorkerDashMapsUITest.java
+++ app/src/androidTest/java/dal/cs/quickCash3/workerAndroidTests/WorkerDashMapsUITest.java
@@ -1,5 +1,6 @@
 package dal.cs.quickCash3.workerAndroidTests;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
@@ -13,28 +14,46 @@ import static org.junit.Assert.assertTrue;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
 
 import org.junit.Before;
 
 import dal.cs.quickCash3.location.WorkerDashboardMapExampleActivity;
 
 @RunWith(AndroidJUnit4.class)
-public class WorkerDashMapsTest {
+public class WorkerDashMapsUITest {
     UiDevice device;
+    private static final int LAUNCH_TIMEOUT = 5000;
+    final String launcherPackage = "dal.cs.quickCash3";
 
     @Before
     public void setup() {
         device = UiDevice.getInstance(getInstrumentation());
 
-        Context context = InstrumentationRegistry.getInstrumentation().getContext();
+        Context context = ApplicationProvider.getApplicationContext();
         Intent launcherIntent = new Intent(context, WorkerDashboardMapExampleActivity.class);
 
         launcherIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(launcherIntent);
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+
+        try {
+            UiObject acceptButton = device.findObject(new UiSelector().text("Allow"));
+            acceptButton.click();
+        } catch (UiObjectNotFoundException e) {
+            // If the accept permission button doesn't exist that's fine for now
+        }
+        try {
+            UiObject mapTab=device.findObject(new UiSelector().descriptionContains("Map"));
+            mapTab.click();
+        } catch (UiObjectNotFoundException e) {
+            // If we can't find the Map tab that means I programmed it wrong...
+        }
     }
 
     @Test
