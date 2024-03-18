4d379f388e06c9de7da7db21a9ee73a040901dbc
Yuki Law <Yuki.Law@dal.ca>
2024-03-18 16:43:55 -0300

    Added tests again and added code to read from database

--- app/src/main/java/dal/cs/quickcash3/payment/WorkerPayPalActivity.java
+++ app/src/main/java/dal/cs/quickcash3/payment/WorkerPayPalActivity.java
@@ -2,6 +2,7 @@ package dal.cs.quickcash3.payment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -9,9 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import dal.cs.quickcash3.R;
+import dal.cs.quickcash3.data.CompletedJob;
+import dal.cs.quickcash3.database.Database;
+import dal.cs.quickcash3.database.DatabaseExampleActivity;
+import dal.cs.quickcash3.database.firebase.MyFirebaseDatabase;
 
 public class WorkerPayPalActivity extends AppCompatActivity {
+    private final CompletedJob job = new CompletedJob();
+    private static final String LOG_TAG = WorkerPayPalActivity.class.getSimpleName();
+    private final AtomicBoolean buttonWaiting = new AtomicBoolean(false);
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +32,30 @@ public class WorkerPayPalActivity extends AppCompatActivity {
 
         // Initialize UI elements
         Button checkworkerPaymentButton = findViewById(R.id.seePayStatus);
-
-        checkworkerPaymentButton.setOnClickListener(view -> moveToPaymentStatusWindow());
+        Database database = new MyFirebaseDatabase();
+        job.readFromDatabase(database,
+                "kawnerv9823fh",
+                () -> {
+                    if (buttonWaiting.get()) {
+                        moveToPaymentStatusWindow();
+                    }
+                },
+                error -> Log.w(LOG_TAG, error));
+        checkworkerPaymentButton.setOnClickListener(view -> {
+            if (job.getPayId() == null) {
+                buttonWaiting.set(true);
+            } else {
+                moveToPaymentStatusWindow();
+            }
+        });
     }
 
     protected void moveToPaymentStatusWindow() {
         Intent paymentStatusIntent = null;
 
         paymentStatusIntent = new Intent(getBaseContext(), WorkerPaymentConfirmationActivity.class);
+        paymentStatusIntent.putExtra("PayID", job.getPayId());
+        paymentStatusIntent.putExtra("Status", job.getStatus());
 
         startActivity(paymentStatusIntent);
     }
--- app/src/main/java/dal/cs/quickcash3/payment/WorkerPaymentConfirmationActivity.java
+++ app/src/main/java/dal/cs/quickcash3/payment/WorkerPaymentConfirmationActivity.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import dal.cs.quickcash3.R;
 
 public class WorkerPaymentConfirmationActivity extends AppCompatActivity {
+    private String status;
+    private String paymentID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +22,11 @@ public class WorkerPaymentConfirmationActivity extends AppCompatActivity {
     private void init() {
         TextView paymentStatus = findViewById(R.id.workerStatusMessage);
         TextView payID = findViewById(R.id.workerPayID);
-        //paymentStatus.setText(status);
-        //payID.setText(paymentID);
+        Intent intent = getIntent();
+        status = intent.getStringExtra("Status");
+        paymentID = intent.getStringExtra("PayID");
+        paymentStatus.setText(status);
+        payID.setText(paymentID);
     }
 
 }
