ee3997ac11e73d3825438e4dd7b7b8f828914335
Yuki Law <Yuki.Law@dal.ca>
2024-03-18 02:46:01 -0300

    Added UI logic

--- app/src/main/java/dal/cs/quickcash3/payment/WorkerPayPalActivity.java
+++ app/src/main/java/dal/cs/quickcash3/payment/WorkerPayPalActivity.java
@@ -1,4 +1,33 @@
 package dal.cs.quickcash3.payment;
 
-public class WorkerPayPalActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import dal.cs.quickcash3.R;
+
+public class WorkerPayPalActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_worker_pay_pal);
+
+        // Initialize UI elements
+        Button checkworkerPaymentButton = findViewById(R.id.seePayStatus);
+
+        checkworkerPaymentButton.setOnClickListener(view -> moveToPaymentStatusWindow());
+    }
+
+    protected void moveToPaymentStatusWindow() {
+        Intent paymentStatusIntent = null;
+
+        paymentStatusIntent = new Intent(getBaseContext(), WorkerPaymentConfirmationActivity.class);
+
+        startActivity(paymentStatusIntent);
+    }
 }
--- app/src/main/java/dal/cs/quickcash3/payment/WorkerPaymentConfirmationActivity.java
+++ app/src/main/java/dal/cs/quickcash3/payment/WorkerPaymentConfirmationActivity.java
@@ -1,4 +1,26 @@
 package dal.cs.quickcash3.payment;
 
-public class WorkerPaymentConfirmationActivity {
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dal.cs.quickcash3.R;
+
+public class WorkerPaymentConfirmationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_worker_payment_confirmation);
+        init();
+    }
+
+    private void init() {
+        TextView paymentStatus = findViewById(R.id.workerStatusMessage);
+        TextView payID = findViewById(R.id.workerPayID);
+        //paymentStatus.setText(status);
+        //payID.setText(paymentID);
+    }
+
 }
