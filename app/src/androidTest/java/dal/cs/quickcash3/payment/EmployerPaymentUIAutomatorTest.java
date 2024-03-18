274ebd36b20402cc2a3f72cf13a2cc63bbf6f55e
Yuki Law <Yuki.Law@dal.ca>
2024-03-18 14:06:07 -0300

    Create UI tests again

---
+++ app/src/androidTest/java/dal/cs/quickcash3/payment/EmployerPaymentUIAutomatorTest.java
@@ -0,0 +1,97 @@
+package dal.cs.quickcash3.payment;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertTrue;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.After;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+
+public class EmployerPaymentUIAutomatorTest {
+    private static final String CONFIRM = "Confirm !";
+    private static final String PAY_CARD = "Pay with Card";
+    private static final String KEYBOARD = "Keyboard";
+    private static final String ALLOW_CAM = "While using the app";
+    private static final String CARD_BOX = "1234 5678";
+    private static final String ACCEPT_CARD = "5317805091683572";
+    private static final String DECLINED_CARD = "5317805091683572";
+    private static final String EXP_BOX = "MM/YY";
+    private static final String DATE = "0825";
+    private static final String CVV_BOX = "123";
+    private static final String CVV = "161";
+    private static final String DONE = "Done";
+    private static final String CHARGE = "Charge Card";
+    private static final String APPROVED = "approved";
+
+
+    @Rule
+    public final ActivityScenarioRule<EmployerPayPalActivity> activityRule =
+            new ActivityScenarioRule<>(EmployerPayPalActivity.class);
+
+    private final UiDevice device = UiDevice.getInstance(getInstrumentation());
+
+    @Test
+    public void checkIfMovedToDashboard() throws UiObjectNotFoundException, InterruptedException {
+        UiObject paymentConfirmButton = device.findObject(new UiSelector().textContains(CONFIRM));
+        paymentConfirmButton.clickAndWaitForNewWindow();
+        UiObject payCardButton = device.findObject(new UiSelector().textContains(PAY_CARD));
+        payCardButton.clickAndWaitForNewWindow();
+        UiObject allowCameraButton = device.findObject(new UiSelector().textContains(ALLOW_CAM));
+        if (allowCameraButton.exists()) {
+            allowCameraButton.clickAndWaitForNewWindow();
+        }
+        UiObject keyboardButton = device.findObject(new UiSelector().textContains(KEYBOARD));
+        keyboardButton.clickAndWaitForNewWindow();
+        UiObject cardNumBox = device.findObject(new UiSelector().textContains(CARD_BOX));
+        cardNumBox.setText(ACCEPT_CARD);
+        UiObject expiresBox = device.findObject(new UiSelector().textContains(EXP_BOX));
+        expiresBox.setText(DATE);
+        UiObject cvvBox = device.findObject(new UiSelector().textContains(CVV_BOX));
+        cvvBox.setText(CVV);
+        UiObject doneButton = device.findObject(new UiSelector().textContains(DONE));
+        doneButton.clickAndWaitForNewWindow();
+        UiObject chargeButton = device.findObject(new UiSelector().textContains(CHARGE));
+        chargeButton.clickAndWaitForNewWindow();
+        Thread.sleep(3000);
+        UiObject approveStatus = device.findObject(new UiSelector().textContains(APPROVED));
+        assertTrue(approveStatus.exists());
+    }
+
+    @Test
+    public void declinedCard() throws UiObjectNotFoundException, InterruptedException {
+        UiObject paymentConfirmButton = device.findObject(new UiSelector().textContains(CONFIRM));
+        paymentConfirmButton.clickAndWaitForNewWindow();
+        UiObject payCardButton = device.findObject(new UiSelector().textContains(PAY_CARD));
+        payCardButton.clickAndWaitForNewWindow();
+        UiObject allowCameraButton = device.findObject(new UiSelector().textContains(ALLOW_CAM));
+        if (allowCameraButton.exists()) {
+            allowCameraButton.clickAndWaitForNewWindow();
+        }
+        UiObject keyboardButton = device.findObject(new UiSelector().textContains(KEYBOARD));
+        keyboardButton.clickAndWaitForNewWindow();
+        UiObject cardNumBox = device.findObject(new UiSelector().textContains(CARD_BOX));
+        cardNumBox.setText(DECLINED_CARD);
+        UiObject expiresBox = device.findObject(new UiSelector().textContains(EXP_BOX));
+        expiresBox.setText(DATE);
+        UiObject cvvBox = device.findObject(new UiSelector().textContains(CVV_BOX));
+        cvvBox.setText(CVV);
+        UiObject doneButton = device.findObject(new UiSelector().textContains(DONE));
+        doneButton.clickAndWaitForNewWindow();
+        UiObject chargeButton = device.findObject(new UiSelector().textContains(CHARGE));
+        chargeButton.clickAndWaitForNewWindow();
+        Thread.sleep(3000);
+        UiObject approveStatus = device.findObject(new UiSelector().textContains(APPROVED));
+        assertTrue(approveStatus.exists());
+    }
+}
