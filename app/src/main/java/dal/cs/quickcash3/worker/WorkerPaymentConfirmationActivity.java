1aec698ee17c19afefe74442267759a4340208e9
Yuki Law <Yuki.Law@dal.ca>
2024-03-18 01:40:49 -0300

    Added UI code

--- app/src/main/java/dal/cs/quickcash3/worker/WorkerPaymentConfirmationActivity.java
+++
@@ -1,8 +0,0 @@
-package dal.cs.quickcash3.worker;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import dal.cs.quickcash3.R;
-
-public class WorkerPaymentConfirmationActivity extends AppCompatActivity {
-}
