5663683721a5383ffed57d78bcf91f64b5486d34
rozee <et218892@dal.ca>
2024-03-20 01:16:04 -0300

    Fixed connected test inconsistencies for search filters.

--- app/src/androidTest/java/dal/cs/quickcash3/search/SearchJobEspressoTest.java
+++ app/src/androidTest/java/dal/cs/quickcash3/search/SearchJobEspressoTest.java
@@ -11,30 +11,55 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
+import static dal.cs.quickcash3.test.ExampleJobList.generateJobPosts;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.rule.GrantPermissionRule;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import dal.cs.quickcash3.R;
+import dal.cs.quickcash3.database.Database;
+import dal.cs.quickcash3.database.mock.MockDatabase;
 import dal.cs.quickcash3.worker.WorkerDashboard;
 
 public class SearchJobEspressoTest {
-
+    private final Context context = ApplicationProvider.getApplicationContext();
     @Rule
     public final ActivityScenarioRule<WorkerDashboard> activityRule =
-            new ActivityScenarioRule<>(WorkerDashboard.class);
+        new ActivityScenarioRule<>(
+            new Intent(context, WorkerDashboard.class)
+                .addCategory(context.getString(R.string.MOCK_DATABASE))
+        );
     @Rule
     public GrantPermissionRule permissionRule =
             GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION);
+    private Database database;
+
+    @Before
+    public void setup() {
+        ActivityScenario<WorkerDashboard> scenario = activityRule.getScenario();
+        scenario.onActivity(activity -> {
+            // Do not run the test if we are not using the mock database.
+            assertTrue("Not using Mock Database", activity.getDatabase() instanceof MockDatabase);
+            database = activity.getDatabase();
+        });
+
+        generateJobPosts(database, Assert::fail);
+    }
 
     @Test
     public void searchResultsTest() {
-
         onView(withId(R.id.workerSearchPage)).perform(click());
         onView(allOf(withClassName(is("android.widget.ImageView")), withContentDescription("Search")))
                 .perform(click());
