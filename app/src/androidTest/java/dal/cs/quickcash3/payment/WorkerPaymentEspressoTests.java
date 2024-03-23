36febadbd52ccda352f6c31d49a832ea5a44b2c4
rozee <et218892@dal.ca>
2024-03-23 12:49:40 -0300

    Increased worker payment espresso test timeout

--- app/src/androidTest/java/dal/cs/quickcash3/payment/WorkerPaymentEspressoTests.java
+++ app/src/androidTest/java/dal/cs/quickcash3/payment/WorkerPaymentEspressoTests.java
@@ -27,6 +27,7 @@ public class WorkerPaymentEspressoTests {
     private final Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
     private final Instrumentation.ActivityMonitor monitor = new Instrumentation.ActivityMonitor(
             WorkerPaymentConfirmationActivity.class.getName(), null, false);
+    private static final int MAX_TIMEOUT = 10000;
 
     @Before
     public void setup() {
@@ -48,7 +49,7 @@ public class WorkerPaymentEspressoTests {
     public void showPaymentStatus() {
         SystemClock.sleep(100);
         onView(withId(R.id.seePayStatus)).perform(click());
-        monitor.waitForActivityWithTimeout(5000);
+        monitor.waitForActivityWithTimeout(MAX_TIMEOUT);
         onView(withId(R.id.workerStatusMessage)).check(matches(withText("approved")));
         onView(withId(R.id.workerPayID)).check(matches(withText("Hello Yuki")));
     }
