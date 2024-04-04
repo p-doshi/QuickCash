9bd68f75730827b715b1b3f54ad0681848ddca06
pdoshi <pr882442@dal.ca>
2024-04-03 21:25:14 -0300

    some changes to connect to database

--- app/src/main/java/dal/cs/quickcash3/jobdetail/ApplyJob.java
+++ app/src/main/java/dal/cs/quickcash3/jobdetail/ApplyJob.java
@@ -9,18 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import dal.cs.quickcash3.R;
 
 
 public class ApplyJob extends Fragment {
 
+    private DatabaseReference databaseReference;
     @Override @NonNull
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                              @NonNull ViewGroup container,
-                              @NonNull Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_apply_job, container, false);
+    public void onCreate(@NonNull Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        this.databaseReference = database.getReference("available_jobs");
     }
 
     @Override
@@ -30,7 +36,21 @@ public class ApplyJob extends Fragment {
         Button button = view.findViewById(R.id.applyForJob);
 
         button.setOnClickListener(v -> {
-            // Handle button click
+            applyForJob();
         });
+    }
+
+    private void applyForJob() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            String jobId = "your_job_id";
+            DatabaseReference applicantsRef = databaseReference.child(jobId).child("applicants").child(userId);
+            applicantsRef.child("UserId").setValue(currentUser.getUid());
+
+
+            Toast.makeText(getContext(), "Applied for job successfully", Toast.LENGTH_SHORT).show();
+        }
     }
 }
