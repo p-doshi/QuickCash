9859a2b710a933d958ded52cf6ceff43d775c42e
rozee <et218892@dal.ca>
2024-04-04 21:51:15 -0300

    Fixed broken CI test

--- app/src/androidTest/java/dal/cs/quickcash3/login/LoginEspressoTest.java
+++ app/src/androidTest/java/dal/cs/quickcash3/login/LoginEspressoTest.java
@@ -8,9 +8,13 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.junit.Assert.assertNull;
+import static dal.cs.quickcash3.test.WaitForAction.waitFor;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -20,50 +24,45 @@ import org.junit.Test;
 import dal.cs.quickcash3.R;
 
 public class LoginEspressoTest {
-    //public ActivityScenario<LoginActivity> scenario;
     @Rule
     public final ActivityScenarioRule<LoginActivity> activityRule =
-            new ActivityScenarioRule<>(LoginActivity.class);
-
-
+        new ActivityScenarioRule<>(LoginActivity.class);
+    private static final long MAX_TIMEOUT = 30000;
 
     @After
     public void teardown() {
         // Just to be sure.
+        assertNull(FirebaseAuth.getInstance().getCurrentUser());
         FirebaseAuth.getInstance().signOut();
     }
 
     @Test
     public void testEmptyEmail() {
-        onView(withId(R.id.emailaddress)).perform(typeText("")).perform(closeSoftKeyboard());
-        onView(withId(R.id.etPassword)).perform(typeText("hi")).perform(closeSoftKeyboard());
+        onView(withId(R.id.etPassword)).perform(typeText("hi"), closeSoftKeyboard());
         onView(withId(R.id.continueButton)).perform(click());
         onView(withId(R.id.statusLabel)).check(matches(withText(R.string.EMPTY_EMAIL_TOAST)));
     }
 
     @Test
     public void testEmptyPassword() {
-        onView(withId(R.id.emailaddress)).perform(typeText("pdoshi@gmail.com")).perform(closeSoftKeyboard());
-        onView(withId(R.id.etPassword)).perform(typeText("")).perform(closeSoftKeyboard());
+        onView(withId(R.id.emailaddress)).perform(typeText("parthdoshi135@gmail.com"), closeSoftKeyboard());
         onView(withId(R.id.continueButton)).perform(click());
         onView(withId(R.id.statusLabel)).check(matches(withText(R.string.EMPTY_PASSWORD_TOAST)));
-        Assert.assertNull(FirebaseAuth.getInstance().getCurrentUser());
     }
 
     @Test
     public void testInvalidEmail() {
-        onView(withId(R.id.emailaddress)).perform(typeText("pdoshigmail.com")).perform(closeSoftKeyboard());
-        onView(withId(R.id.etPassword)).perform(typeText("hahapranked")).perform(closeSoftKeyboard());
+        onView(withId(R.id.emailaddress)).perform(typeText("pdoshigmail.com"));
+        onView(withId(R.id.etPassword)).perform(typeText("hahapranked"), closeSoftKeyboard());
         onView(withId(R.id.continueButton)).perform(click());
         onView(withId(R.id.statusLabel)).check(matches(withText(R.string.INVALID_EMAIL_TOAST)));
     }
 
     @Test
-    public void testInvalidCredentials() throws InterruptedException {
-        onView(withId(R.id.emailaddress)).perform(typeText("parthdoshi135@gmail.com")).perform(closeSoftKeyboard());
-        onView(withId(R.id.etPassword)).perform(typeText("hahapranked")).perform(closeSoftKeyboard());
+    public void testInvalidCredentials() {
+        onView(withId(R.id.emailaddress)).perform(typeText("parthdoshi135@gmail.com"));
+        onView(withId(R.id.etPassword)).perform(typeText("hahapranked"), closeSoftKeyboard());
         onView(withId(R.id.continueButton)).perform(click());
-        Thread.sleep(2000);
-        onView(withId(R.id.statusLabel)).check(matches(withText("Wrong email or password")));
+        onView(withId(R.id.statusLabel)).perform(waitFor(withText(R.string.INVALID_CREDENTIALS), MAX_TIMEOUT));
     }
 }
