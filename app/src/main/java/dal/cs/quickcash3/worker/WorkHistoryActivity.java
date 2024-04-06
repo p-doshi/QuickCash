47cea4e9a32d26208ff430b75afadfc8bd147384
guandong <gn533549@dal.ca>
2024-04-05 22:03:09 -0300

    some error

--- app/src/main/java/dal/cs/quickcash3/worker/WorkHistoryActivity.java
+++ app/src/main/java/dal/cs/quickcash3/worker/WorkHistoryActivity.java
@@ -2,14 +2,22 @@ package dal.cs.quickcash3.worker;
 
 import static dal.cs.quickcash3.database.DatabaseDirectory.COMPLETED_JOBS;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,65 +25,72 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
 
 import dal.cs.quickcash3.R;
 import dal.cs.quickcash3.data.CompletedJob;
 import dal.cs.quickcash3.database.Database;
+import dal.cs.quickcash3.search.RegexSearchFilter;
 import dal.cs.quickcash3.search.SearchFilter;
 import dal.cs.quickcash3.util.AsyncLatch;
 
 public class WorkHistoryActivity extends AppCompatActivity {
 
-    /*private double totalIncome;
+    private double totalIncome;
     private double totalRating;
     RecyclerView jobHistoryList;
     TextView tvTotalIncome;
     TextView tvAverageReputation;
-    private List<CompletedJob> jobHistories = new ArrayList<>();
+    private final List<CompletedJob> jobList = new ArrayList<>();
     JobAdapter adapter;
-    private  AsyncLatch<SearchFilter<CompletedJob>> asyncFilter;
-    private  Database database;
-    private final AtomicInteger callbackId = new AtomicInteger();
-    private static final String LOG_TAG = CompletedJobListFragment.class.getSimpleName();
-    private final Map<String, CompletedJob> completedJobMap = new HashMap<>();
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_history);
         init();
-        fetchHistories();
-        adapter = new JobAdapter(jobHistories);
+        adapter = new JobAdapter(jobList);
         jobHistoryList.setAdapter(adapter);
         jobHistoryList.setLayoutManager(new LinearLayoutManager(this));
-        tvTotalIncome.setText(getString(R.string.total_income, String.format(Locale.US, "%.2f", totalIncome)));
-        tvAverageReputation.setText(getString(R.string.average_reputation, String.format(Locale.US, "%.2f", totalRating / jobHistories.size())));
     }
 
     private void init() {
         jobHistoryList = findViewById(R.id.jobRecyclerView);
         tvTotalIncome = findViewById(R.id.totalIncome);
         tvAverageReputation = findViewById(R.id.averageReputation);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("public").child("completed_jobs");
+        fetchJobsFromDatabase();
     }
-    //TUDO
-    private void fetchHistories(){
-        asyncFilter.get(searchFilter -> {
-            Log.d(LOG_TAG, "Restarting database search listener");
-            database.removeListener(this.callbackId.get());
-            completedJobMap.clear();
 
-            int callbackId = database.addSearchListener(COMPLETED_JOBS.getValue(), CompletedJob.class, searchFilter, // 修改为COMPLETED_JOBS和CompletedJob
-                    (key, job) -> {
-                        Log.v(LOG_TAG, key + ": " + job);
-                        if (job == null) {
-                            completedJobMap.remove(key);
-                        } else {
-                            this.completedJobMap.put(key, job);
-                            adapter.addJob(job);
-                        }
-                    },
-                    error -> Log.w(LOG_TAG, "received database error: " + error));
-            this.callbackId.set(callbackId);
-        });
-    }*/
+    private void fetchJobsFromDatabase() {
+        String workerId = ".*"; // TODO: Make this an actual worker ID.
+
+        RegexSearchFilter<CompletedJob> searchFilter = new RegexSearchFilter<>(CompletedJob::getWorker);
+        searchFilter.setPattern(Pattern.compile(workerId));
+
+        ObjectSearchAdapter<CompletedJob> searchAdapter = new ObjectSearchAdapter<>(searchFilter);
+        searchAdapter.addObserver(new CustomObserver<>(this::addJob, this::removeWorker));
+
+        callbackId = database.addDirectoryListener(CompletedJob.DIR, CompletedJob.class, searchAdapter::receive,
+                error -> {
+                    // TODO: handle errors. You can probably just log it and move on.
+                });
+    }
+
+    private void addJob(@NonNull CompletedJob job) {
+        jobList.add(job);
+        // TODO: handle new jobs here.
+    }
+
+    private void removeWorker(@NonNull CompletedJob job) {
+        jobList.remove(job);
+        // TODO: handle deleted jobs here
+    }
+
+    private void updateUI(){
+        tvTotalIncome.setText(getString(R.string.total_income, String.format(Locale.US, "%.2f", totalIncome)));
+        tvAverageReputation.setText(getString(R.string.average_reputation,
+                jobList.size() > 0 ? String.format(Locale.US, "%.2f", totalRating / jobList.size()) : "0"));
+    }
 }
